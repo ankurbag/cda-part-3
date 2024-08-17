b6656c94f6be3592401bcfeb75467416a6e25785
Ankur Bag <bagankur@gmail.com>
2024-08-16 22:39:50 -0700

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
-
+app.use(cors());
 const warehouseRoutes = require("./routes/warehouseRoute");
 const inventoryRoutes = require("./routes/inventoryRoute");
 
 app.get("/", (_req, res) => {
-  res.send("Welcome to my API");
+	res.send("Welcome to my API");
 });
 
 app.use("/warehouses", warehouseRoutes);
 app.use("/inventories", inventoryRoutes);
 
 app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
+	console.log(`running at http://localhost:${PORT}`);
 });
